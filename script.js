df50f8ef43727f4046a58ecb9641a304787f971c
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-11-06 00:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+	function volume_sphere() {
+	    //Write your code here
+		const radius = Number(document.querySelector('#radius').value);
+		const volume = document.querySelector('#volume');
+	
+		if(radius < 0 || Number.isNaN(radius)) {
+			volume.value = 'NaN';
+			return;
+		}
+	
+		const vol = ((4/3) * Math.PI * Math.pow(radius, 3)).toFixed(4);
+	
+		volume.value = vol;
+	
+	} 
+	
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('MyForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    volume_sphere();
+  });
+});
